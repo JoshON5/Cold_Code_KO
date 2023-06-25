61ff9e177132ff0dcaa4eaa23a962c763630791e
Clark <joshua@DOOMBOT-2.local>
2023-06-24 22:43:18 -0500

    changed up the id's of the multiple choice buttons so they would appear as buttons when the quiz starts. Also added functions to start the quiz and timer

--- develop/script.js
+++ develop/script.js
@@ -6,8 +6,10 @@ let questions = document.querySelector(".questions");
 let questionCount = 0;
 let finalScreen = document.querySelector(".quiz-final-screen");
 let userInitials = document.querySelector(".initials");
-let scores = document.querySelector(".highscores");
+let rank = document.querySelector(".highscores");
 let scoreRanks = [];
+let answerCheck = document.querySelector("#validation");
+let score = document.querySelector("#score");
 
 const leaders = document.querySelector("#view-board");
 const quizBtn = document.querySelector("#start");
@@ -18,4 +20,74 @@ const btn2 = document.querySelector("#answer-2");
 const btn3 = document.querySelector("#answer-3");
 const subBtn = document.querySelector("#submit");
 const playAgain = document.querySelector("#play-again");
-const clearScores = document.querySelector("#erase-leaders")
+const clearScores = document.querySelector("#erase-leaders");
+
+const codeQuest = [
+  {
+    question: "Which one of these is NOT a data type present in JavaScript?",
+    answers: ["A. Booleans", "B. Numbers", "C. Null", "D. Queries"],
+    correctAnswer: "3",
+  },
+  {
+    question: "When creating an array with objects, the code is enclosed in?",
+    answers: [
+      "A. Curly Brackets",
+      "B. Brackets",
+      "C. Quotes",
+      "D. Parentheses",
+    ],
+    correctAnswer: "1",
+  },
+  {
+    question:
+      "To link your JavaScript external file to your HTML, you put it where? ",
+    answers: ["A. Head", "B. Div", "C. End of Body", "D. Start of Body"],
+    correctAnswer: "2",
+  },
+  {
+    question:
+      "In Chrome Dev Tools, where can you log variables, functions, typeof etc.?",
+    answers: ["A. Console", "B. Elements", "C. VSCode", "D. YouTube"],
+    correctAnswer: "0",
+  },
+  {
+    question: "The (++) operator increases in increments of?",
+    answers: ["A. 1", "B. 5", "C. 3", "D. 4"],
+    correctAnswer: "0",
+  },
+];
+
+function countdown() {
+  let timeInterval = setInterval(function () {
+    seconds--;
+    timeEl.textContent = seconds + "s";
+
+    if (seconds === 0 || questionCount === codeQuest.length) {
+      clearInterval(timeInterval);
+      questions.style.display = "none";
+      finalScreen.style.display = "block";
+      score.style.display = seconds;
+    }
+  }, 1000);
+}
+
+function playQuiz() {
+    intro.style.display = "none";
+    questions.style.display = "inline";
+    questionCount = 0;
+
+    countdown();
+    setQuestion(questionCount);
+}
+
+function setQuestion(id) {
+    if (id < codeQuest.length) {
+        questions.textContent = codeQuest[id].question;
+        btn0.textContent = codeQuest[id].answers[0];
+        btn1.textContent = codeQuest[id].answers[1];
+        btn2.textContent = codeQuest[id].answers[2];
+        btn3.textContent = codeQuest[id].answers[3];
+    }
+}
+
+quizBtn.addEventListener("click", playQuiz);
